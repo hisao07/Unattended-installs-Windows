13d798b2a18103f85429c61252fd813d73b31081
hodorsec <hodorsec@protonmail.com>
2023-03-18 20:27:24 +0100

    Used Chocolatey; WIP

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -5,4 +5,15 @@ echo %PATH%
 :: Install all the things; for example:
 choco install /y 7zip
 choco install /y notepadplusplus
-choco install /y boxstarter.winconfig
+choco install /y vscode
+choco install /y dnspy
+choco install /y putty
+choco install /y processhacker
+choco install /y winscp
+choco install /y wireshark
+choco install /y firefox
+choco install /y googlechrome
+choco install /y python3
+choco install /y sysinternals
+choco install /y treesizefree
+:: choco install /y office365proplus
